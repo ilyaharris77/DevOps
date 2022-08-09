f29872cbe637db4c6ac1efbab52a018115af12fb
IlyaTsoi <45252974+IlyaTsoi@users.noreply.github.com>
2022-08-09 10:04:28 +0300

    Update backup_fdb.sh
    getLastTag rewritten using running_backups.txt

--- backup_fdb.sh
+++ backup_fdb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 backupPath=/root/backupname2
 logFile=/root/backupname2/log
+nfsMountPath=/mnt/backup_fdb
 backupDuration=30
 deltaTime=3600
 lastTag=''
@@ -32,6 +33,7 @@ function stopBackup {
   removeTmp
   if ! [ -z "$success" ]; then
     toLog "$success"
+    sed -i "/$tag/d" $nfsMountPath/runing_backups.txt
     return 0
   elif ! [ -z "$failed" ]; then
     toLog "$failed" 31
@@ -42,8 +44,10 @@ function stopBackup {
 function getLastTag {
   local count
   local lastTag
-  count=$(fdbcli --exec "status details" | awk -v RS='' '/Running backup tags/' | sed 1d | wc -l)
-  lastTag=$(fdbcli --exec "status details" | awk -v RS='' '/Running backup tags/' | sed 1d | awk 'NR == 1{print$1}')
+  count=$(cat $nfsMountPath/runing_backups.txt | wc -l)
+  lastTag=$(cat $nfsMountPath/runing_backups.txt | tail -n 1)
+  #count=$(fdbcli --exec "status details" | awk -v RS='' '/Running backup tags/' | sed 1d | wc -l)
+  #lastTag=$(fdbcli --exec "status details" | awk -v RS='' '/Running backup tags/' | sed 1d | awk 'NR == 1{print$1}')
   if [[ $count -gt 1 ]]; then
     toLog "More than one backup running" 31
   fi
@@ -57,6 +61,7 @@ function startBackup {
   local failed=$(cat $backupPath/tmp2) 
   removeTmp
   if ! [ -z "$success" ]; then
+    echo $tag >> $nfsMountPath/runing_backups.txt
     toLog "$success"
     return 0
   elif ! [ -z "$failed" ]; then
